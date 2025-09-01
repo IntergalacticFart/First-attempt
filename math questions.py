bac74f07f1580f2ed9744f5575d4bde5e1688332
IntergalacticFart <luizaogsilva09@gmail.com>
2025-08-31 20:22:41 -0400

    Add files via upload

---
+++ math questions.py
@@ -0,0 +1,20 @@
+um: int = 5
+dois: int = 6
+result: int = um * dois
+print ("The answer to your query is: ", result)
+tres: int = 8
+quatro: int = 3
+result_two: int = tres / quatro
+print ("The answer to your query is: ", result_two)
+result_three: int = tres % quatro
+print ("The answer to your query is: ", result_three)
+result_four: int = tres % quatro
+print ("The answer to your query is: ", result_four)
+cinco: int = 4200000
+seis: int = 23090
+result_five: int = cinco + seis
+print ("The answer to your query is: ", result_five)
+sete: int = 7633
+result_six: int = cinco - sete
+print ("The answer to your query is: ", result_six)
+print ("Luiz Henrique Giacomini da Silva V04293778")
