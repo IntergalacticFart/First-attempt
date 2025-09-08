b515ea2c0fe599b20b9eff79acfe775b1083dee3
IntergalacticFart <luizaogsilva09@gmail.com>
2025-09-07 22:50:00 -0400

    Add files via upload

---
+++ math questions corrected.py
@@ -0,0 +1,30 @@
+print("enter one number at each line for the multiplication")
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+result = num1 * num2
+print("the answer to your querry is:", result)
+print("enter one number at each line for the division")
+num3 = int(input("enter the first number: "))
+num4 = int(input("enter the second number: "))
+result2 = num3 / num4
+print("the answer to your querry is:", result2)
+print("enter one number at each line for the division without remainder")
+num5 = int(input("enter the first number: "))
+num6 = int(input("enter the second number: "))
+result3 = num5 % num6
+print("the answer to your querry is:", result3)
+print("enter one number at each line for the modolo")
+num7 = int(input("enter the first number: "))
+num8 = int(input("enter the second number: "))
+result4 = num7 % num8
+print("the answer to your querry is:", result4)
+print("enter one number at each line for the addition")
+num9 = int(input("enter the first number: "))
+num10 = int(input("enter the second number: "))
+result5 = num9 + num10
+print("the answer to your querry is:", result5)
+print("enter one number at each line for the subtraction")
+num11 = int(input("enter the first number: "))
+num12 = int(input("enter the second number: "))
+result6 = num11 - num12
+print("the answer to your querry is:", result6)
